8f2aac8a96f6ee6135c28c708c3a2daadec9dca4
samuel dias <93612090+SDias14@users.noreply.github.com>
2023-03-03 16:53:29 -0300

    testes do curso

---
+++ banco/encript/testes/index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$pau = "-";
+//no primeiro for, ele vai repetir 10 vezes no sentido vertical
+for ($i = 0; $i <= 9; $i++) {
+   //no segundo for, ele vai repetir 10 vezes no sentido horizontal
+    for ($j = 0; $j <= 9; $j++) {
+        echo $pau;
+    }
+    echo "<br>";
+}
+
+
+?>
